4bb24677c75676617aefcc7b344f9aafa56fb65c
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-12-09 10:05:05 +0800

    Update gemini.js

--- modules/scripts/commands/gemini.js
+++ modules/scripts/commands/gemini.js
@@ -8,7 +8,7 @@ module.exports.config = {
     category: "ai",
     description: "Test send message text",
     adminOnly: false, // if false all users can use this command but if true only the admin id lists on config.json can use
-    usePrefix: false,, // if true it uses the command with the prefix example /testsendimage-url but if false no need prefix can direct use testsendimage-url
+    usePrefix: false, // if true it uses the command with the prefix example /testsendimage-url but if false no need prefix can direct use testsendimage-url
     cooldown: 5, // cooldown on how many command to be used
 };
 
